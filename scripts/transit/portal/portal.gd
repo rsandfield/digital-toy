d1594c6b6c4190bdd4011fbb1b3bec0a63689528
Robert Sandfield <robert.f.sandfield@gmail.com>
2024-11-04 06:14:01 -0800

    Lint portal scripts

--- scripts/transit/portal/portal.gd
+++ scripts/transit/portal/portal.gd
@@ -3,12 +3,21 @@
 class_name Portal
 extends Area3D
 
+
+const CAM_NEAR_NEEDED_TO_PREVENT_GLITCH = 0.001
+const MESH_DUPLICATE_RETAIN_TIME_MS = 5000
+
+# If a body moved further than this while passing through the portal, consider it a teleport.
+# Disables portal movement in cases where the player teleports via some ability, but moved
+# over the portal border in the process. Should only activate portal when walking through.
+const MOVE_WAS_TELEPORT_THRESHOLD = 5.0
+
 @export var portal_id : String
 @export var other_portal_id : String
-var other_portal : Node3D
 
-## The visual layer the portal is rendered on. Set this to something other than the layer your level and models are on.
-## This is used to hide the destination portal from the camera's view so we can see past it.
+## The visual layer the portal is rendered on. Set this to something other than
+## the layer your level and models are on. This is used to hide the destination
+## portal from the camera's view so we can see past it.
 @export var cull_layer : int = 4
 
 @export var make_mesh_duplicates : bool = true
@@ -22,18 +31,16 @@ var other_portal : Node3D
 ## only solution I found was to lower near cull plane on camera
 @export var enable_camera_near_plane_fix: bool = true
 
+var other_portal : Node3D
 var is_in_world: bool = true
 
 var portal_visual : MeshInstance3D
 var camera_viewport : SubViewport
 var viewport_camera : Camera3D
 
-const CAM_NEAR_NEEDED_TO_PREVENT_GLITCH = 0.001
-
-# If a body moved further than this while passing through the portal, consider it a teleport.
-# Disables portal movement in cases where the player teleports via some ability, but moved
-# over the portal border in the process. Should only activate portal when walking through.
-const MOVE_WAS_TELEPORT_THRESHOLD = 5.0
+# Mesh duplicate cache
+# Helps performance and seems to make the transition from portal to portal slightly smoother
+var mesh_duplicate_cache = [] #[body, mesh_duplicate, store_time]
 
 var _tracked_phys_bodies = []
 
@@ -42,7 +49,7 @@ func set_other_portal(other: Portal):
         # Purge current tracked bodies
         var i = len(_tracked_phys_bodies) - 1
         while i >= 0:
-            other_portal._remove_tracked_phys_body(_tracked_phys_bodies[i].body)
+            other_portal.remove_tracked_phys_body(_tracked_phys_bodies[i].body)
             i -= 1
 
     other_portal = other
@@ -55,14 +62,15 @@ func set_other_portal(other: Portal):
         # Bring it home so it doesn't get cleaned up
         camera_viewport.reparent(self)
         return
-    
+
     viewport_camera.set_cull_mask_value(other_portal.cull_layer, false)
     if get_viewport() != other_portal.get_viewport():
         camera_viewport.reparent(other_portal)
     else:
         camera_viewport.reparent(self)
 
-# Edge case but possible. Adding this to prevent teleporting twice if body lands exactly on portal plane
+# Edge case but possible. Adding this to prevent teleporting twice if body lands
+# exactly on portal plane
 func _nonzero_sign(value):
     var s = sign(value)
     if s == 0:
@@ -99,12 +107,10 @@ func _ready():
         return
     portal_visual.set_layer_mask_value(1, false)
     portal_visual.set_layer_mask_value(cull_layer, true)
-    # Godot normally does order _process/_phys_process -> internal physics step moving bodies -> draw immediately
-    # We must call do_updates on the frame_pre_draw signal or there will be 1 frame where the body has not actually teleported
-    # This will mess up the view and cause flicker for 1 frame on  player controllers
-    #RenderingServer.frame_pre_draw.connect(do_updates)
-    # Or so I would think. It only works with this + _process + _physics_process all calling do_updates.
-    # Otherwise there is flicker. I'm not sure why just frame_pre_draw doesn't work.
+    # We must call do_updates on the frame_pre_draw signal or there will be 1
+    # frame where the body has not actually teleported. This will mess up the
+    # view and cause flicker for 1 frame. I'm not sure why just frame_pre_draw
+    # doesn't work.
     _update_portal_area_size()
     _set_portal_camera_environment_to_world3d_environment_no_tonemap()
 
@@ -137,7 +143,7 @@ func _update_portal_area_size():
 
     if portal_visual.mesh is BoxMesh:
         portal_visual.position = portal_pos
-    
+
     var barrier_size = Vector3(
         0.2,
         self.size.y + self.portal_area_y_margin * 2,
@@ -153,7 +159,7 @@ func _update_portal_area_size():
         0.1)
     $LeftBarrier/CollisionShape3D.shape.size = barrier_size
 
-# Copied from https://github.com/V-Sekai/avatar_vr_demo/blob/master/addons/V-Sekai.xr-mirror/mirror.gd
+# https://github.com/V-Sekai/avatar_vr_demo/blob/master/addons/V-Sekai.xr-mirror/mirror.gd
 func set_projection_oblique_near_plane(matrix: Projection, clip_plane: Plane):
     # Based on the paper
     # Lengyel, Eric. “Oblique View Frustum Depth Projection and Clipping”.
@@ -181,24 +187,29 @@ func set_projection_oblique_near_plane(matrix: Projection, clip_plane: Plane):
     matrix.w.z = c.w - matrix.w.w
     return matrix
 
-# This function is based on https://github.com/SebLague/Portals/blob/master/Assets/Scripts/Core/Portal.cs
+# https://github.com/SebLague/Portals/blob/master/Assets/Scripts/Core/Portal.cs
 func _update_portal_camera_near_clip_plane(camera):
     if not camera.has_method("set_override_projection"):
         return # Needs https://github.com/V-Sekai/godot/tree/override_projection_4.2 branch
-    
+
     const NEAR_CLIP_OFFSET = 0.05
     const NEAR_CLIP_LIMIT = 0.1
-    
+
     # Calculate the near clip plane in camera space
     var clip_plane = other_portal.global_transform
     var clip_plane_forward: Vector3 = -clip_plane.basis.z
-    var portal_side = _nonzero_sign(clip_plane_forward.dot(other_portal.global_transform.origin - camera.global_transform.origin))
- 
+    var portal_side = _nonzero_sign(clip_plane_forward.dot(
+        other_portal.global_transform.origin - camera.global_transform.origin
+    ))
+
     var cam_space_pos = camera.get_camera_transform().affine_inverse() * clip_plane.origin
-    var cam_space_normal = (camera.get_camera_transform().affine_inverse().basis * clip_plane_forward) * portal_side
+    var cam_space_normal = (
+        camera.get_camera_transform().affine_inverse().basis * clip_plane_forward
+    ) * portal_side
     var cam_space_dst = - cam_space_pos.dot(cam_space_normal) + NEAR_CLIP_OFFSET;
-    
-    # Oblique plane when very close to portal causes glitching/visual artifacts, so only enable if a small distance away
+
+    # Oblique plane when very close to portal causes glitching/visual artifacts,
+    # so only enable if a small distance away
     if abs(cam_space_dst) > NEAR_CLIP_LIMIT:
         var proj : Projection = camera.get_camera_projection()
         var near_clip_plane = Plane(cam_space_normal, cam_space_dst)
@@ -206,17 +217,24 @@ func _update_portal_camera_near_clip_plane(camera):
         camera.set_override_projection(proj)
     else:
         # Set back to unmodified frustum if camera is very close to portal
-        camera.set_override_projection(Projection(Vector4.ZERO, Vector4.ZERO, Vector4.ZERO, Vector4.ZERO))
-    
+        camera.set_override_projection(
+            Projection(Vector4.ZERO, Vector4.ZERO, Vector4.ZERO, Vector4.ZERO)
+        )
+
 func _update_camera_to_other_portal():
     var cur_camera = get_viewport().get_camera_3d()
     if not cur_camera or not other_portal:
         return
-        
+
     # first, get the relative position/rotation of the camera to this portal
-    var cur_camera_transform_rel_to_this_portal = self.global_transform.affine_inverse() * cur_camera.global_transform
-    var moved_to_other_portal = other_portal.global_transform * cur_camera_transform_rel_to_this_portal
-    # then, set the portal camera's transform to that relative position/rotation, but relative to other_portal
+    var cur_camera_transform_rel_to_this_portal = (
+        self.global_transform.affine_inverse() * cur_camera.global_transform
+    )
+    var moved_to_other_portal = (
+        other_portal.global_transform * cur_camera_transform_rel_to_this_portal
+    )
+    # then, set the portal camera's transform to that relative position/rotation,
+    # but relative to other_portal
 
     viewport_camera.global_transform = moved_to_other_portal
     viewport_camera.fov = cur_camera.fov
@@ -272,7 +290,7 @@ func _thicken_portal_if_necessary_to_prevent_camera_near_cull():
 
 func do_updates():
     $CollisionShape3D.disabled = not self.visible
-    
+
     for body in _get_bodies_which_passed_through_this_frame():
         if body.is_multiplayer_authority():
             _move_to_other_portal(body)
@@ -282,9 +300,9 @@ func do_updates():
             # Added to prevent visual glitching for multiplayer peers.
             # Try to detect when peer has swapped places with their mesh duplicate and then
             # move it to the appropriate position.
-            _remove_tracked_phys_body(tracked_body.body)
-            other_portal._add_tracked_phys_body(tracked_body.body)
-            
+            remove_tracked_phys_body(tracked_body.body)
+            other_portal.add_tracked_phys_body(tracked_body.body)
+
     # Note: placing these after above so portal thickening/camera update happen same frame as move
     _clear_mesh_duplicate_cache()
     _update_camera_to_other_portal()
@@ -298,7 +316,7 @@ func _process(_delta):
         _update_portal_area_size()
         return
     do_updates()
-    
+
 func _physics_process(_delta):
     do_updates()
 
@@ -306,32 +324,38 @@ func _try_detect_portal_pass_through_on_multiplayer_peer(tracked_body):
     if not tracked_body.mesh_duplicator:
         return false
     const DISTANCE_THRESHOLD_FROM_DUPLICATE = 2.5
-    var dist_from_dupe = (tracked_body.body.global_position - tracked_body.mesh_duplicator.dupe.global_position).length()
+    var dist_from_dupe = (
+        tracked_body.body.global_position -
+        tracked_body.mesh_duplicator.dupe.global_position
+    ).length()
     #if dist_from_dupe < DISTANCE_THRESHOLD_FROM_DUPLICATE:
     #    print("DETECTED OTHER PEER TELEPORT, dist was "+str(dist_from_dupe))
     return dist_from_dupe < DISTANCE_THRESHOLD_FROM_DUPLICATE
 
 func _remove_hanging_body_check():
     # Code to prevent edge case, mostly for multiplayer
-    # It's possible you can skip the Area3D on the other side of portal if moving too fast/teleporting
-    # Since we need to hand object off to other portal same frame we remove it from this on port,
-    # do this check to prevent handing off an object that is outside of the area3d, leaving it lingering
+    # It's possible you can skip the Area3D on the other side of portal if
+    # moving too fast/teleporting. Since we need to hand object off to other
+    # portal same frame we remove it from this on port, do this check to prevent
+    # handing off an object that is outside of the area3d, leaving it lingering
     var i = len(_tracked_phys_bodies) - 1
     while i >= 0:
         var tracked_body = _tracked_phys_bodies[i].body
         var track_duration = Time.get_ticks_msec() - _tracked_phys_bodies[i].track_start_time
         if track_duration > 250.0 and not overlaps_body(tracked_body):
-            _remove_tracked_phys_body(tracked_body)
+            remove_tracked_phys_body(tracked_body)
             print("%s removed tracked body [%s] for edge case" % [portal_id, tracked_body])
         i -= 1
-    
+
 func _move_to_other_portal(body: PhysicsBody3D):
     if not other_portal:
         return
 
     print("%s moving %s to %s" % [portal_id, body, other_portal_id])
-    
-    var transform_rel_to_this_portal = self.global_transform.affine_inverse() * body.global_transform
+
+    var transform_rel_to_this_portal = (
+        self.global_transform.affine_inverse() * body.global_transform
+    )
     var moved_to_other_portal = other_portal.global_transform * transform_rel_to_this_portal
     body.global_transform = moved_to_other_portal
 
@@ -346,9 +370,9 @@ func _move_to_other_portal(body: PhysicsBody3D):
             .rotated(Vector3(1, 0, 0), r.x) \
             .rotated(Vector3(0, 1, 0), r.y) \
             .rotated(Vector3(0, 0, 1), r.z)
-    
-    _remove_tracked_phys_body(body)
-    var newly_tracked_body = other_portal._add_tracked_phys_body(body)
+
+    remove_tracked_phys_body(body)
+    var newly_tracked_body = other_portal.add_tracked_phys_body(body)
     # We just moved the body. If it was a player, the camera may have been moved.
     # So we must do updates on the other portal. The camera and thickening will need to be updated.
     # No guarantee other portal has done updates for this frame already or not.
@@ -371,11 +395,7 @@ func _get_bodies_which_passed_through_this_frame():
         # Once we're done set position_last_frame again
         tracked_body.position_last_frame = pos_node.global_position
     return bodies_that_passed_through
-    
-# Mesh duplicate cache
-# Helps performance and seems to make the transition from portal to portal slightly smoother
-var mesh_duplicate_cache = [] #[body, mesh_duplicate, store_time]
-const MESH_DUPLICATE_RETAIN_TIME_MS = 5000
+
 
 func _clear_mesh_duplicate_cache():
     var mesh_duplicate_cache_to_remove = []
@@ -392,9 +412,9 @@ func _clear_mesh_duplicate_cache():
 func _store_mesh_duplicate_in_cache(body, mesh_duplicate):
     mesh_duplicate_cache.push_back([body, mesh_duplicate, Time.get_ticks_msec()])
 
-func _get_mesh_duplicate_from_cache(body, ask_other_portal = false):
+func get_mesh_duplicate_from_cache(body, ask_other_portal = false):
     if ask_other_portal and other_portal:
-        var other_result = other_portal._get_mesh_duplicate_from_cache(body)
+        var other_result = other_portal.get_mesh_duplicate_from_cache(body)
         if other_result != null:
             return other_result
     for i in range(len(mesh_duplicate_cache)):
@@ -403,19 +423,18 @@ func _get_mesh_duplicate_from_cache(body, ask_other_portal = false):
             mesh_duplicate_cache.remove_at(i)
             return item[1]
     return null
-            
+
 func _make_or_get_mesh_duplicate(body):
-    var cache_result = _get_mesh_duplicate_from_cache(body, true)
+    var cache_result = get_mesh_duplicate_from_cache(body, true)
     if cache_result:
         cache_result.in_portal = self
         cache_result.out_portal = other_portal
         return cache_result
-    else:
-        var new_mesh_duplicator = PortalMeshDuplicator.new()
-        new_mesh_duplicator.body = body
-        new_mesh_duplicator.in_portal = self
-        new_mesh_duplicator.out_portal = other_portal
-        return new_mesh_duplicator
+    var new_mesh_duplicator = PortalMeshDuplicator.new()
+    new_mesh_duplicator.body = body
+    new_mesh_duplicator.in_portal = self
+    new_mesh_duplicator.out_portal = other_portal
+    return new_mesh_duplicator
 
 func _get_tracked_phys_body_entry(body):
     for entry in _tracked_phys_bodies:
@@ -423,7 +442,7 @@ func _get_tracked_phys_body_entry(body):
             return entry
     return null
 
-func _add_tracked_phys_body(body):
+func add_tracked_phys_body(body):
     # First check if we already have the body or not in our list
     var tracked_body_entry = _get_tracked_phys_body_entry(body)
     if tracked_body_entry != null:
@@ -446,29 +465,32 @@ func _add_tracked_phys_body(body):
         if enable_camera_near_plane_fix:
             newly_tracked_body.camera.near = CAM_NEAR_NEEDED_TO_PREVENT_GLITCH
     _tracked_phys_bodies.push_back(newly_tracked_body)
-    
-    if body.has_method("_on_portal_tracking_enter"):
-        body._on_portal_tracking_enter(self)
+
+    if body.has_method("on_portal_tracking_enter"):
+        body.on_portal_tracking_enter(self)
     if body.get_viewport() != get_viewport():
         body.reparent(get_viewport())
     print("%s entered by %s" % [portal_id, body])
     return newly_tracked_body
-    
-func _remove_tracked_phys_body(body):
+
+func remove_tracked_phys_body(body):
     for i in len(_tracked_phys_bodies):
         if _tracked_phys_bodies[i].body == body:
             print("%s removed body %s" % [portal_id, body])
-            
+
             if _tracked_phys_bodies[i].mesh_duplicator:
                 if is_instance_valid(_tracked_phys_bodies[i].mesh_duplicator):
                     other_portal.remove_child(_tracked_phys_bodies[i].mesh_duplicator)
-                    _store_mesh_duplicate_in_cache(_tracked_phys_bodies[i].body, _tracked_phys_bodies[i].mesh_duplicator)
+                    _store_mesh_duplicate_in_cache(
+                        _tracked_phys_bodies[i].body,
+                        _tracked_phys_bodies[i].mesh_duplicator
+                    )
             if _tracked_phys_bodies[i].camera:
                 _tracked_phys_bodies[i].camera.near = _tracked_phys_bodies[i].prev_camera_near
             _tracked_phys_bodies.remove_at(i)
-            
-            if body.has_method("_on_portal_tracking_leave"):
-                body._on_portal_tracking_leave(self)
+
+            if body.has_method("on_portal_tracking_leave"):
+                body.on_portal_tracking_leave(self)
             return
 
 func find_by_class(node: Node, name_of_class : String):
@@ -482,9 +504,9 @@ func find_by_class(node: Node, name_of_class : String):
 
 # This shapecast is necessary because _on_body_entered and _on_body_exited may be fired late in some
 # edge cases Which could cause the conundrum of _on_body_entered being called late, after body has
-# left, or _on_body_exited being called late, like firing the same frame it was added. Causing 
+# left, or _on_body_exited being called late, like firing the same frame it was added. Causing
 # incorrectly adding or removing a body. Something like that is going on. This fixes the bug of
-# teleporting up to the roof when you rotate the exit velocity by PI on the y axis and it 
+# teleporting up to the roof when you rotate the exit velocity by PI on the y axis and it
 # rubberbands very quickly between the 2 portals. May happen rarely even without the incorrect y
 # rotation set
 func _check_shapecast_collision(body):
@@ -497,15 +519,19 @@ func _check_shapecast_collision(body):
     return false
 
 func _on_body_entered(body):
-    # Disable non-moving static bodes from teleporting (except AnimatableBody3Ds which are considered static).
-    # CSGShape3Ds are also static if you enable their use_collision property so disable them.
-    if (not body.is_class("StaticBody3D") or body.is_class("AnimatableBody3D")) and not body.is_class("CSGShape3D"):
+    # Disable non-moving static bodes from teleporting (except AnimatableBody3Ds
+    # which are considered static). CSGShape3Ds are also static if you enable
+    # their use_collision property so disable them.
+    if (
+        (!body.is_class("StaticBody3D") || body.is_class("AnimatableBody3D")) &&
+        !body.is_class("CSGShape3D")
+    ):
         if other_portal and _check_shapecast_collision(body):
-            _add_tracked_phys_body(body)
+            add_tracked_phys_body(body)
 
 func _on_body_exited(body):
     if not _check_shapecast_collision(body) or $CollisionShape3D.disabled:
-        _remove_tracked_phys_body(body)
+        remove_tracked_phys_body(body)
 
 func _to_string():
     return "Portal[%s]" % [portal_id]
--- scripts/transit/portal/portal_mesh_duplicator.gd
+++ scripts/transit/portal/portal_mesh_duplicator.gd
@@ -6,15 +6,15 @@ var dupe
 var in_portal
 var out_portal
 
-func find_by_class(node: Node, className : String, exit_after_one = false, result_array = []):
-    if node.is_class(className):
+func find_by_class(node: Node, name_of_class : String, exit_after_one = false, result_array = []):
+    if node.is_class(name_of_class):
         result_array.push_back(node)
         if exit_after_one:
             return result_array
     for child in node.get_children():
-        find_by_class(child, className, exit_after_one, result_array)
+        find_by_class(child, name_of_class, exit_after_one, result_array)
     return result_array
-    
+
 func get_child_by_name(node: Node, child_name):
     for c in node.get_children():
         if c.name == child_name:
@@ -31,7 +31,7 @@ func iterate_through_pair_and_copy_over_props(node: Node, other_node: Node, prop
         var other_node_child = get_child_by_name(other_node, child.name)
         if node_child and other_node_child:
             iterate_through_pair_and_copy_over_props(node_child, other_node_child, props_to_set)
-    
+
 func make_copy_of_node_tree(original : Node, allowed_node_types=[], external_skeleton=null):
     var cur_node
     if allowed_node_types.has(original.get_class()):
@@ -46,16 +46,16 @@ func make_copy_of_node_tree(original : Node, allowed_node_types=[], external_ske
             cur_node.transform = original.transform
             cur_node.visible = original.visible
             cur_node.visibility_parent = original.visibility_parent
-            
+
     if cur_node.is_class("BoneAttachment3D") and external_skeleton:
         cur_node.use_external_skeleton = true
         cur_node.external_skeleton = external_skeleton
     if cur_node.is_class("MeshInstance3D") and external_skeleton:
         cur_node.skeleton = external_skeleton
-    
+
     for child in original.get_children():
         cur_node.add_child(make_copy_of_node_tree(child, allowed_node_types, external_skeleton))
-    
+
     return cur_node
 
 func _ready():
@@ -63,14 +63,17 @@ func _ready():
     var external_skeleton = null
     if len(original_skeletons) > 0:
         external_skeleton = original_skeletons[0].get_path()
-        
+
     dupe = make_copy_of_node_tree(body, ["MeshInstance3D", "CSGShape3D"], external_skeleton)
     add_child(dupe)
     synchronize_all()
 
 func synchronize_all():
     iterate_through_pair_and_copy_over_props(body, dupe, ["transform", "visible"])
-    var original_rel_to_in_portal = in_portal.global_transform.affine_inverse() * body.global_transform
+    var original_rel_to_in_portal = (
+        in_portal.global_transform.affine_inverse() *
+        body.global_transform
+    )
     if not out_portal:
         return
     var moved_to_out_portal = out_portal.global_transform * original_rel_to_in_portal
